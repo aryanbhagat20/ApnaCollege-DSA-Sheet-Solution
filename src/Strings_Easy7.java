583a5402c9c74536de27bf8253309bb3cc6817a8
Aryan Bhagat <aryanbhagat20@gmail.com>
2025-03-19 01:26:17 +0530

    19th March 2025

--- src/Strings_Easy7.java
+++ src/Strings_Easy7.java
@@ -1,2 +1,28 @@
+import java.util.* ;
+//Print all the duplicate characters in a string - Ola + Amdocs IQ Interview Qs
+//GFG
+//Time : O(n) and Space : O(1) for the Fixed size map.
+
 public class Strings_Easy7 {
+    public static void printDuplicateCharactersWithCount(String str) {
+        Map<Character, Integer> charCountMap = new HashMap<>();
+
+        for(char ch : str.toCharArray()){
+            charCountMap.put(ch, charCountMap.getOrDefault(ch , 0) + 1);
+        }
+
+        // Print characters with frequency > 1 (duplicates)
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + ", count = " + entry.getValue());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "GeeksforGeeks";
+        printDuplicateCharactersWithCount(str);
+    }
 }
+
+
